c1bcb50470b0d5e39d068b5b267066d775795e60
Sreehari P S <92331340+sreeharipslog@users.noreply.github.com>
2024-11-05 16:16:07 +0530

    Additional changes

--- src/main/scala/lectures/basics/Function.scala
+++ src/main/scala/lectures/basics/Function.scala
@@ -1,9 +1,11 @@
 package lectures.basics
 
-/** Chapter: 3, Functions
- */
+import scala.annotation.tailrec
 
-/* Functions in Scala can be written in 2 syntax 1). Method syntax 2).
+/** Chapter: 3, Functions
+  */
+
+/* Functions in Scala can be written in two syntax 1). Method syntax 2).
  * Function/val syntax
  */
 
@@ -11,33 +13,37 @@ package lectures.basics
 object Function extends App {
   println("Functions in Scala")
 
-  // Type infered Function
+  // Type inferred Function
   def add2Number(a: Int, b: Int): Int = a + b
+
   // factorial function
   println("factorial of number 25")
+  // 1. Regular recursive function, results in StackOverflow exception
+  def factorial(num: Int): BigInt = if (num == 1) 1 else num * factorial(num - 1)
 
-  def factorial(num: Int): Int = if (num == 1) 1 else num * factorial(num - 1)
-
+  // Tail recursive implementation of factorial
+  @tailrec
+  def tailFactorial(n: Int, accumulator: BigInt = 1): BigInt =
+    if (n <= 0) accumulator else tailFactorial(n - 1, n * accumulator)
   println(factorial(25))
+  println(tailFactorial(25))
 
-  // Auxilary functions
+  // Auxiliary functions
   def simpleFunction(str0: String): Unit = {
     println("This is a simple function")
-
     def aux(str1: String): String = str0 + " " + str1
-
     println(aux("World"))
   }
-
   simpleFunction("Hello")
 
-  // Prime checker funciton
+  // Prime checker function
   def isPrime(n: Int): Boolean = {
+    @tailrec
+    // This becomes a tail recursive function because of the short circuit && operator
+    // Scala compiler optimizes short circuit operation without the need of extra stack frames.
     def isPrimeUntil(num: Int): Boolean =
       if (num <= 1) true else (n % num != 0) && isPrimeUntil(num - 1)
-
     isPrimeUntil(n / 2)
   }
-
   println(s"395 * 6546 is prime? Ans: ${isPrime(395 * 6546)}")
 }

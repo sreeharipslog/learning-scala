f68d0ad304d7182b779fc898cb64625200c42e84
Sreehari P S <92331340+sreeharipslog@users.noreply.github.com>
2024-11-11 06:52:57 +0530

    String Interpolation theory

--- src/main/scala/lectures/basics/StringOps.scala
+++ src/main/scala/lectures/basics/StringOps.scala
@@ -26,12 +26,23 @@ object StringOps extends App {
 
   // String Interpolator
   // S interpolator
+  // s-strings are strings which have variable directly inside of them
   println(s"This is an example of 's' interpolator in scala = $intString")
   println(s"This is an example of 's' interpolator in scala = ${intString + aString}")
   // F Interpolator. Similar to S interpolator, applies formatted string on top. printf style formatting
+  // f strings are simple formatted strings
   println(f"This is an example of 'f' interpolator. Int = ${42}%d and Double = ${65.654654}%4.2f")
   // RAW Interpolator. Similar to S interpolator but print string literally. Variables and Values escaped
+  // raw string: No escaping of literals as in s strings
   println(raw"This is an example of 'raw' interpolator \n\n\n\n\n")
   val escapedString = "This is 2nd example of 'raw' interpolator \n\n Welcome to the Jungle"
   println(raw"$escapedString") // escapes
+
+  // Theory
+  /*
+  * Interpolation works by using StringContext case class. All interpolated strings are processed
+  * strings that calls `id` (s, f, raw) method in StringsContext class with literal strings as their arguements.
+  * eg: s"Hello, This is $name"  --> StringContext("Hello, This is ", "").s(name)
+  * To create custom interpolator add an implicit class that adds new interpolator to StringContext.
+   */
 }
